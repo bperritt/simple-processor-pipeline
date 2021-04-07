f662c2ab56509d810654393d09c7ae5ffb5f1ad0
bperritt <80059774+bperritt@users.noreply.github.com>
2021-04-07 16:23:19 -0400

    Update eightbit_alu.v

--- eightbit_alu.v
+++ eightbit_alu.v
@@ -1,22 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/02/2021 03:53:42 PM
-// Design Name: 
-// Module Name: eightbit_alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Engineer: Bradley Perritt (perritt.b@northeastern.edu)
 //////////////////////////////////////////////////////////////////////////////////
 
 
